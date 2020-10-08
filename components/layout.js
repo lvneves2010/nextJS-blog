2a2d2a621df12923a7b7ba8ccf68d5b18d56ff84
leonardo neves <leonardo.neves@ibm.com>
2020-10-08 19:15:58 -0300

    prerendering

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Leo Neves'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Meu Blog Too'
 
 export default function Layout({ children, home }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -18,6 +28,20 @@ export default function Home() {
         <h1 className="title">
           Aqui está  <Link href="/posts/first-post"><a>meu primeiro post</a></Link>
         </h1>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
